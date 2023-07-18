5fa63db870ce28bdc124b49ed9b568eea957d095
Michael Nunan <michael.nunan@outlook.com>
2023-07-17 19:20:20 -0500

    author details showing

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,7 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { viewAuthorDetails } from '../../api/mergedData';
 
-export default function ViewAuthor() {
+export default function ViewBook() {
+  const [authorDetails, setAuthorDetails] = useState({});
   const router = useRouter();
+
   const { firebaseKey } = router.query;
-  return firebaseKey;
+
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="text-white ms-5 details">
+        <h5>
+          {authorDetails.first_name}{authorDetails.last_name}
+        </h5>
+        Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
+        <hr />
+        <p>
+          {authorDetails.favorite ? '🤍' : ''}
+        </p>
+      </div>
+    </div>
+  );
 }
