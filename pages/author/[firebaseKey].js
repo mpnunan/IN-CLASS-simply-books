bb42ee4d5741aa4ebf24ea3d78268bf1ccd38a15
Michael Nunan <michael.nunan@outlook.com>
2023-07-17 20:21:48 -0500

    got author books showing

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,9 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
-export default function ViewBook() {
+export default function ViewAuthor() {
   const [authorDetails, setAuthorDetails] = useState({});
   const router = useRouter();
 
@@ -14,16 +14,23 @@ export default function ViewBook() {
   }, [firebaseKey]);
 
   return (
-    <div className="mt-5 d-flex flex-wrap">
-      <div className="text-white ms-5 details">
-        <h5>
-          {authorDetails.first_name}{authorDetails.last_name}
-        </h5>
-        Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
-        <hr />
-        <p>
-          {authorDetails.favorite ? '🤍' : ''}
-        </p>
+    <div>
+      <div className="mt-5 d-flex flex-wrap">
+        <div className="text-white ms-5 details">
+          <h5>
+            {authorDetails.first_name}{authorDetails.last_name}
+          </h5>
+          Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
+          <hr />
+          <p>
+            {authorDetails.favorite ? '🤍' : ''}
+          </p>
+        </div>
+      </div>
+      <div className="d-flex flex-wrap">
+        {authorDetails.books?.map((book) => (
+          <BookCard key={book.firebaseKey} bookObj={book} onUpdate={console.warn('easter egg')} />
+        ))}
       </div>
     </div>
   );
